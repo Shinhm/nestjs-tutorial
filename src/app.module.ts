46b067ee26d8d6733de5f0e09773e2aabed3fe12
dave-s <dave.s@kakaocorp.com>
2022-01-01 16:40:14 +0900

    nestjs 튜토리얼 middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { DogsModule } from './dogs/dogs.module';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [CatsModule, DogsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude({ path: 'cats', method: RequestMethod.POST })
+      .forRoutes('cats', 'dogs');
+  }
+}
